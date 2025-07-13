981d8ebbce634da55ab6965b1c891cca6e91b846
Krishna26-Dev <kamsalikrishnakumar2002@gmail.com>
2025-07-13 23:53:53 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -1,32 +1,24 @@
-const e = require('express');
-const express = require('express');
-const {userAuth, adminAuth} = require('../middlewares/auth'); // Importing the auth middleware
+const e = require("express");
+const express = require("express");
 const app = express(); // Instance of Express application
 
-
-//Handle Authentication for all routes GET, POST, PUT, DELETE
-// app.use("/admin", adminAuth); // Apply adminAuth middleware to all routes starting with /admin
-// app.use("/user", userAuth);
-
-app.post('/user/login', (req, res) => {
-  res.send("User logged in successfully");
-}
-)
-
-app.get('/users/data', userAuth,  (req, res) => {
-  console.log("Handling the users route!!");
-  res.send("User data sent");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong ");
+  }
 });
 
-app.get('/admin/getAllUsers', adminAuth,  (req, res) => {
-  res.send("All Data sent");
+app.get("/getUserData", (req, res) => {
+  try {
+    //Logic of DB Call and get user data
+
+    throw new Error("Error in getting user data");
+    res.send("User data sent");
+  } catch (error) {
+    res.status(500).send(err.message);
+  }
 });
 
-
-  app.get('/admin/deleteUser', (req, res) => {
-  res.send("Deleted a user");
-});
-
-app.listen(8888, () => {
-  console.log(`Server is running on port 8888`);
+app.listen(7777, () => {
+  console.log(`Server is running on port 7777`);
 }); // Start the server on port 3000
